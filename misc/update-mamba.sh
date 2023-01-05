3b8d21600e67945882dfe95aabf5aa731dae4c99
Lai Quang Huy <64073540+1qh@users.noreply.github.com>
2023-01-05 18:14:30 +0000

    .

--- misc/update-mamba.sh
+++ misc/update-mamba.sh
@@ -1,4 +1,4 @@
-micromamba install py-xgboost
+micromamba install py-xgboost -n base
 
 micromamba install arrow apscheduler aria2 awswrangler bentoml binaryornot black bpython catboost channels cookiecutter dash datefinder deepchecks diffusers django-cleanup django-cors-headers django-environ django-filter djangorestframework django-rq dj-database-url dtreeviz dvc easydict einops evidently ffmpy flake8-bugbear fugue gdown great-expectations gunicorn httpie icecream imageio imbalanced-learn inflect jinja2-time jupyter-book kaggle lightning-bolts lightning-flash matplotx maya micro mlxtend mysqlclient neptune-lightgbm newt opencv pandarallel pandera paramiko parse pip-autoremove prefect pycryptodome pydub pylint-django pymongo pyspark python-decouple python-duckdb python-graphviz python-magic pytorch-ignite spacy streamlit-pandas-profiling swifter tensorboardx torchinfo torchvision transformers visidata wandb watchfiles websockets whitenoise workalendar yapf youtube-dl -c conda-forge -n base
 
